4f70744e3ed286c566113b58e5eb82c22626eba9
YonanPrasetyo <yonanprasetyo91792@gmail.com>
2025-01-21 17:08:38 +0700

    Chapter 5 Build a Web Server

--- logEvents.js
+++ logEvents.js
@@ -5,15 +5,15 @@ const fs = require("fs");
 const fsPromises = require("fs").promises;
 const path = require("path");
 
-const logEvents = async (message) => {
-    const dateTime = `${format(new Date(), "dd/MM/yyyy\tHH:mm:ss")}`
+const logEvents = async (message, logName) => {
+    const dateTime = `${format(new Date(), "yyyy/MM/dd\tHH:mm:ss")}`
     const logItem = `${dateTime}\t${uuid()}\t${message}\n`
     console.log(logItem)
     try{
         if(!fs.existsSync(path.join(__dirname, "logs"))){
             await fsPromises.mkdir(path.join(__dirname, 'logs'))
         }
-        await fsPromises.appendFile(path.join(__dirname, "logs", "eventLog.txt"), logItem)
+        await fsPromises.appendFile(path.join(__dirname, "logs", logName), logItem)
     }catch(err){
         console.log(err)
     }
