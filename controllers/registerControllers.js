c47f7465a23c8d2684aaa006e2ac24d09e9d1dc3
YonanPrasetyo <yonanprasetyo91792@gmail.com>
2025-01-27 23:43:28 +0700

    Chapter: 14: Mongoose Data Models

--- controllers/registerControllers.js
+++ controllers/registerControllers.js
@@ -1,10 +1,4 @@
-const userDB = {
-    users: require("../model/users.json"),
-    setUsers: function (data) {this.users = data}
-}
-
-const fsPromises = require("fs").promises
-const path = require("path")
+const User = require("../model/User");
 const bcrypt = require("bcrypt")
 
 const handleNewUser = async (req, res) => {
@@ -12,25 +6,23 @@ const handleNewUser = async (req, res) => {
     if (!user || !pwd) return res.status(400).json({ "message": "Username and Password are required" })
     
     // check for duplicate usernames in the db
-    const duplicate = userDB.users.find(person => person.username === user)
-    if (duplicate) return res.status(409).json({ "message": "usermane already exists"}) //Conflict
+    const duplicate = await User.findOne({ username: user }).exec()
+    if (duplicate) return res.sendStatus(409) //Conflict
 
     try{
         // encrypt the password
         const hashedPwd = await bcrypt.hash(pwd, 10)
-        // store the new user
-        const newUser = {
+
+        // create and store the new user
+        const result = await User.create({
             "username": user,
-            "roles": { "user": 2001 },
             "password": hashedPwd
-        }
-        userDB.setUsers([...userDB.users, newUser])
+        })
+
         
-        await fsPromises.writeFile(
-            path.join(__dirname, "..", "model", "users.json"),
-            JSON.stringify(userDB.users)
-        )
-        console.log(userDB.users)
+
+        console.log(result)
+        
         res.status(200).json({ "success": `New user ${user} created` })
     }catch(err){
         res.status(500).json({ "message": err.message })
